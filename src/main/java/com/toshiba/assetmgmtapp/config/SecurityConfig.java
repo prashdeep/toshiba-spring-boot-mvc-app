eaf257b1e75476d864e5049df77f7f87cbcc6f14
prashdeep <pradeep.kumar44@gmail.com>
2019-11-22 18:02:53 +0530

    committing the changes

--- src/main/java/com/toshiba/assetmgmtapp/config/SecurityConfig.java
+++ src/main/java/com/toshiba/assetmgmtapp/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.toshiba.assetmgmtapp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,12 +14,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/v1/assets/**")
+                .antMatchers(HttpMethod.GET, "/v1/assets/**")
                 .hasAnyRole("ADMIN","USER")
-                .antMatchers("/v1/admin/**")
+                .antMatchers(HttpMethod.GET, "/v1/admin/**")
                 .hasRole("ADMIN")
                 .and()
                 .httpBasic();
+
     }
 
     @Override
@@ -32,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("vinay")
                 .password("admin")
                 .roles("USER","ADMIN");
+
 
     }
 
