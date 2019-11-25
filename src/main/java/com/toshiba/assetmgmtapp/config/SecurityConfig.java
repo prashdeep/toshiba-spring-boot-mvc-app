9006585d7fa2b92c76fd30f2027ffb2f4429e50d
prashdeep <pradeep.kumar44@gmail.com>
2019-11-25 10:25:06 +0530

    committing the changes with userdetails service

--- src/main/java/com/toshiba/assetmgmtapp/config/SecurityConfig.java
+++ src/main/java/com/toshiba/assetmgmtapp/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.toshiba.assetmgmtapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService domainUserDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -25,18 +31,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .inMemoryAuthentication()
-                .withUser("kiran")
-                .password("welcome")
-                .roles("USER")
-                .and()
-                .withUser("vinay")
-                .password("admin")
-                .roles("USER","ADMIN");
 
-
+        /*
+                authenticationManagerBuilder
+                        .inMemoryAuthentication()
+                        .withUser("kiran")
+                        .password("welcome")
+                        .roles("USER")
+                        .and()
+                        .withUser("vinay")
+                        .password("admin")
+                        .roles("USER","ADMIN");
+        */
+        authenticationManagerBuilder.userDetailsService(domainUserDetailsService);
     }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
---
+++ src/main/java/com/toshiba/assetmgmtapp/security/DomainUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.toshiba.assetmgmtapp.security;
+
+import com.toshiba.assetmgmtapp.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DomainUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public DomainUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
